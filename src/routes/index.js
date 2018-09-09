9fc684c3c4ee2aba2e952bba2bd1b2548afe6f8a
Yoshiya Hinosawa <stibium121@gmail.com>
2018-09-09 12:52:55 +0900

    refactor: users-me.js -> users-self.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,6 @@ const { checkJwt } = require('../util/jwt')
 const { corsOk, allowMethods, ok } = require('../util/routes')
 
 module.exports = app => {
-  app.get('/users/self', corsOk, checkJwt, require('./users-me'))
+  app.get('/users/self', corsOk, checkJwt, require('./users-self'))
   app.options('/users/self', corsOk, allowMethods('GET'), ok)
 }
