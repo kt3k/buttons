02f67bde11d5461be0c95596317e80f33865f1cb
Yoshiya Hinosawa <stibium121@gmail.com>
2018-09-30 20:04:16 +0900

    feat: add healthcheck api

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 const express = require('express')
 const app = express()
 const db = require('./util/db')
+const { ok, allowMethods } = require('./util/routes')
 const bodyParser = require('body-parser')
 
 db.on('open', () => console.log('db connected'))
 
 app.use(bodyParser.json())
+
+app.options('/', allowMethods('GET'), ok)
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'ok' })
+})
+
 require('./routes')(app)
 
 app.listen(process.env.PORT || '3000')
