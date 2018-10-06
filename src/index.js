fdc093763a4de60c3e0221842d2f6ab061970f38
Yoshiya Hinosawa <stibium121@gmail.com>
2018-10-06 19:23:35 +0900

    feat: add morgan

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ const express = require('express')
 const app = express()
 const db = require('./util/db')
 const bodyParser = require('body-parser')
+const morgan = require('morgan')
 
 db.on('open', () => console.log('db connected'))
 
 // Uncomment this line if you like to simulate slow api responses.
 // app.use((req, res, next) => setTimeout(next, 3000))
 
+app.use(morgan('tiny'))
 app.use(bodyParser.json())
 
 require('./routes')(app)
