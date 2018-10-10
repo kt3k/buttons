e388958e88d89ca3250ffc3ce997d24b7352c0a0
Yoshiya Hinosawa <stibium121@gmail.com>
2018-10-10 14:42:51 +0900

    feat: show buttons

--- frontend/comps/orgs/user-profile.js
+++ frontend/comps/orgs/user-profile.js
@@ -10,6 +10,10 @@ class UserProfile {
   get displayNameLabel () {}
   @wired('.user-display-id')
   get displayIdLabel () {}
+
+  @wired('.user-profile-buttons-area')
+  get buttonsArea () {}
+
   async __mount__ () {
     const id = getDisplayId(location, false)
 
@@ -18,6 +22,28 @@ class UserProfile {
     this.picture.src = user.picture
     this.displayNameLabel.textContent = user.displayName
     this.displayIdLabel.textContent = user.displayId
+
+    this.fillButtons(user.buttons)
+  }
+
+  fillButtons (buttons) {
+    if (buttons.length === 0) {
+      this.buttonsArea.textContent = 'No buttons'
+    }
+
+    buttons.forEach(button => {
+      const p = document.createElement('p')
+      const span = document.createElement('span')
+      span.classList.add('button')
+      span.textContent = button.name
+      p.appendChild(span)
+      this.buttonsArea.appendChild(p)
+      if (button.description) {
+        const p = document.createElement('p')
+        p.textContent = button.description
+        this.buttonsArea.appendChild(p)
+      }
+    })
   }
 }
 
