73c692747c4377b17be2e7e3795f92a11ba8ee91
Yoshiya Hinosawa <stibium121@gmail.com>
2018-10-10 11:32:16 +0900

    refactor: add layout template

--- bulbofile.js
+++ bulbofile.js
@@ -1,13 +1,16 @@
 const bulbo = require('bulbo')
 const { asset } = bulbo
 
+const layout1 = require('layout1')
 const nunjucks = require('gulp-nunjucks-render')
+const frontmatter = require('gulp-front-matter')
 const bundle = require('bundle-through')
 
 const src = 'frontend'
 
 const paths = {
   html: `${src}/*.html`,
+  layout: `${src}/layout.njk`,
   css: `${src}/css/**/*.css`,
   vendor: `${src}/vendor/**/*.*`,
   js: {
@@ -23,7 +26,14 @@ asset(paths.js.entrypoint)
   .watch(paths.js.src)
   .pipe(bundle({ transform: [['envify', { global: true }], 'babelify'] }))
 
-asset(paths.html).pipe(nunjucks({ basepath: process.env.BASEPATH || '' }))
+asset(paths.html)
+  .pipe(frontmatter({ property: 'fm' }))
+  .pipe(nunjucks({ basepath: process.env.BASEPATH || '' }))
+  .pipe(
+    layout1.nunjucks(paths.layout, {
+      data: { basepath: process.env.BASEPATH || '' }
+    })
+  )
 
 asset(paths.css)
 asset(paths.vendor)
