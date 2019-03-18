83091d35ff907bfeb43df19ba1c80b7eb0315715
Yoshiya Hinosawa <stibium121@gmail.com>
2019-03-18 22:38:10 +0900

    dev: exit server when db connection error

--- src/index.js
+++ src/index.js
@@ -5,6 +5,15 @@ const bodyParser = require('body-parser')
 const morgan = require('morgan')
 
 db.on('open', () => console.log('db connected'))
+db.on('error', e => {
+  if (e.name === 'MongoNetworkError') {
+    console.log('Error: Unable to connect to mongodb')
+    console.log('Check if you run `saku mongo` when development')
+  } else {
+    console.log(e)
+  }
+  process.exit(1)
+})
 
 // Uncomment this line if you like to simulate slow api responses.
 // app.use((req, res, next) => setTimeout(next, 3000))
